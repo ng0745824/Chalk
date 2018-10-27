46bdc25f367735604789aeaf09b9040cea5bf310
Natalena Groom <ng0745824@otc.edu>
2018-10-27 16:19:21 -0500

    story commit

---
+++ story.js
@@ -0,0 +1,50 @@
+const chalk = require('chalk');
+console.log(chalk.magenta('The Dare - A short story from Scary Stories to Tell in the Dark'));
+console.log('\nOne ' + chalk.yellow('night') + ', in a small ' + chalk.red('Ame') + chalk.white('ri') + chalk.blue('can') 
+            + ' town, a young girl threw a party and invited all her friends from ' + chalk.black.bgYellow('school.')
+            + '\nThere was a large group of boys and girls, and, as the ' + chalk.yellow('night') 
+            + ' went on, they began telling scary stories.');
+console.log('\nOne boy said he had heard an urban legend about an old ' + chalk.gray.bgWhiteBright('graveyard') + ' that was located near the girl’s house.'
+            + ' According to the story, an old woman had been buried in the middle of the' + chalk.gray.bgWhiteBright('cemetery') 
+            + ' and there were rumors that she was a ' + chalk.green('witch.') 
+            + ' They said that if someone stood on her ' + chalk.gray('grave') + ' at ' + chalk.yellow('midnight') 
+            + ', she would grab them and drag them down to ' + chalk.bgRed('hell.'));
+console.log(chalk.bgBlackBright('\n"I would never go near that graveyard after dark"') + ', another boy said.');
+console.log(chalk.bgBlackBright('\n"You’re all a bunch of morons"') + ', laughed the girl who owned the house. '
+            + chalk.bgBlackBright('“It’s just a silly superstition. I can’t believe you’d take it seriously.”'));
+console.log('\nThe boy who had told the story turned to her and sneered, ' 
+            + chalk.bgBlackBright('“It’s all very well to be brave when you’re safe and warm in your own home. \nI think you’d have a different attitude if you actually went to that graveyard.”'));
+console.log(chalk.bgBlackBright('\n“It wouldn’t make a difference”') + ', replied the girl haughtily.');
+console.log(chalk.bgBlackBright('\n“Ok, then prove it!”') + ' said the boy. '
+            + chalk.bgBlackBright('“We’ll all go down to the cemetery and wait outside while you go in and stand on the old '
+            + chalk.green('witch’s') + ' grave.”'));
+console.log(chalk.bgBlackBright('\n“A grave doesn’t scare me,”') + ' said the girl.' + chalk.bgBlackBright('“I’ll do it right now.”'));
+console.log(chalk.bgBlackBright('\n“You’ll have to prove you really did it,”') + ' said the boy.'
+            + chalk.bgBlackBright('“Otherwise you might chicken out.”'));
+console.log('\nHe took a ' + chalk.red('knife') + ' from the kitchen drawer and handed it to her.');
+console.log(chalk.bgBlackBright('\n“Stick this ' + chalk.red('knife') + ' in the ' + chalk.black('grave') + ',”') + ' he said.' 
+            + chalk.bgBlackBright('“Then we’ll know you were there for sure.”'));
+console.log('\nThe group of kids set out for the ' + chalk.gray.bgWhiteBright('graveyard') + ' and when they reached the old ' 
+            + chalk.gray('iron gates') + ', they gathered in a circle. All of them stared at the girl.\nShe tried to pretend she wasn’t scared and she hoped they didn’t see her '
+            + chalk.cyan('shivering') + ' in fear.');
+console.log('\nWith the ' + chalk.red('knife') + ' clutched tightly in her hand, the girl walked through the old ' 
+            + chalk.gray('iron gateway') + ' and made her way through the ' + chalk.white.bgBlackBright('darkened') + ' ' 
+            + chalk.gray.bgWhiteBright('graveyard.') + ' In the ' + chalk.black.bgYellowBright('moonlight') 
+            + ', the ' 
+            + chalk.gray.bgWhiteBright('tombstones') + '\nand the ' + chalk.greenBright('trees') + ' cast strange and unnerving '
+            + chalk.gray('shadows.') + ' Eventually, she came to the old woman’s ' + chalk.gray.bgWhiteBright('grave.'));
+console.log(chalk.bgBlackBright('\n“There is nothing to be scared of,”') + ' the girl told herself. ' 
+            + chalk.bgBlackBright('“It’s just a stupid story.”'));
+console.log('\nCrouching in front of the ' + chalk.gray.bgWhiteBright('headstone') + ', she whispered, ' + chalk.bgBlackBright('“I’m not afraid of you.”'));
+console.log('\nThen she raised the ' + chalk.red('knife') + ' above her head and thrust it into the ' + chalk.greenBright('earth.'));
+console.log(chalk.bgBlackBright('\n“Take that, you old ' + chalk.green('witch') + ',”') + ' she chuckled.');
+console.log('\nThe girl was about to turn and leave, when she felt something holding her back. She couldn’t move. Something was tugging on the hem of her dress and dragging her down.');
+console.log('\nShe was filled with terror and flew into a panic.' + chalk.bgBlackBright('“Help!”') + ' she cried. '
+            + chalk.bgBlackBright('“She’s got me! She’s got me!”'));
+console.log('\nWhen she didn’t come back, her friends began to get worried. After a while, they cautiously entered the ' 
+            + chalk.gray.bgWhiteBright('graveyard') + ' to look for her. They found her sprawled across the old woman’s ' 
+            + chalk.gray.bgWhiteBright('grave.') + ' She was dead, and her face was ' + chalk.cyanBright('frozen') + ' in a silent scream.');
+console.log('\nWithout realizing it, she had plunged the ' + chalk.red('knife') 
+            + ' through her skirt and had pinned it to the ground. That was what had been holding her.');
+console.log('\nThe poor girl had been so terrified that her ' + chalk.red('heart') + ' gave out and she died of ' + chalk.magenta('fright.'));
+            
